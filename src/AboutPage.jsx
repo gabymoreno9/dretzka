98ba7a2408d3bf4c382b3115657196e1fd853183
Gabriela Moreno <gabrielaxmoreno@gmail.com>
2020-05-25 14:23:43 -0700

    text for the about page is complete

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -6,8 +6,39 @@ class AboutPage extends React.Component {
     render = () =>
         <PageWithNavBar mode="dark" className="about-us-page">
             <div className="about-us-title">
-                The team behind Dretzka & Associates
+                The team behind Dretzka & <br/>Associates
             </div>
+            <br/>
+
+            <div className="our-history">
+                Our History
+                <p>Founded in 1985, Dretzka and <br/>
+                Associates have grown into a <br/>
+                powerfully dynamic and reputable <br/>
+                organization serving Northern <br/>
+                California, Western Nevada and <br/>
+                Hawaii.</p>
+            </div>
+            <br/>
+            
+            <div className="our-mission">
+                Our Mission
+                <p>Our goals have remained the same as <br/>
+                they were int he beginning:<br/>
+                <br/>
+                Represent the very best of companies
+                <br/>
+                <br/>
+                Serve them better than they expect
+                <br/>
+                <br/>
+                Provide the very best of service and <br/>
+                training to our costumers
+                
+                
+                </p>
+            </div>
+
         </PageWithNavBar>
 }
 
